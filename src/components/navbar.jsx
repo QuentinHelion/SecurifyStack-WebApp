f777452b8323c8a362afec5b172f833a9e240a5f
Quentin Hélion <helion.quentin@gmail.com>
2024-02-15 18:53:42 +0100

    add basic navbar & add bootstrap to project

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+
+    return (
+        <div id="navbar" class="">
+            <div class="nav-div p-3">
+                <a class="nav-btn">bla bla bla</a>
+            </div>
+
+            <div class="nav-div p-3">
+                <a class="nav-btn">bla bla bla</a>
+            </div>
+
+            <div class="nav-div p-3">
+                <a class="nav-btn">bla bla bla</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
